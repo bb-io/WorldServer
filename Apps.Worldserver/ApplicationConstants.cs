720ef78d86552d9d9efa854f5e1b1236111d6c2b
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-09-02 12:43:38 +0300

    Cleanup

--- Apps.Worldserver/ApplicationConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Apps.Worldserver;
-
-public class ApplicationConstants
-{
-    public const string ClientId = "#{WEBFLOW_CLIENT_ID}#";
-    public const string ClientSecret = "#{WEBFLOW_SECRET}#";
-    public const string Scope = "#{WEBFLOW_SCOPE}#";
-}
