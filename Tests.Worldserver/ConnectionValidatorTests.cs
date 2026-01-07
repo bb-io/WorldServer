0fd214e3bf0ce27355f38eb7b7411c7c835e258d
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-01-07 18:43:47 +0200

    add tests project

---
+++ Tests.Worldserver/ConnectionValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Tests.Worldserver.Base;
+using Apps.Worldserver.Connections;
+using Blackbird.Applications.Sdk.Common.Authentication;
+
+namespace Tests.Worldserver;
+
+[TestClass]
+public class ConnectionValidatorTests : TestBase
+{
+    [TestMethod]
+    public async Task ValidateConnection_ValidData_ShouldBeSuccessful()
+    {
+        // Arrange
+        var validator = new ConnectionValidator();
+
+        // Act
+        var result = await validator.ValidateConnection(Creds, CancellationToken.None);
+
+        // Assert
+        Console.WriteLine(result.Message);
+        Assert.IsTrue(result.IsValid);
+    }
+
+    [TestMethod]
+    public async Task ValidateConnection_InvalidData_ShouldFail()
+    {
+        // Arrange
+        var validator = new ConnectionValidator(); 
+        var newCredentials = Creds.Select(x => new AuthenticationCredentialsProvider(x.KeyName, x.Value + "_incorrect"));
+
+        // Act
+        var result = await validator.ValidateConnection(newCredentials, CancellationToken.None);
+
+        // Assert
+        Console.WriteLine(result.Message);
+        Assert.IsFalse(result.IsValid);
+    }
+}
---
+++ Tests.Worldserver/ProjectActionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Tests.Worldserver.Base;
+using Apps.Worldserver.Actions;
+using Apps.Worldserver.Models.Projects.Request;
+
+namespace Tests.Worldserver;
+
+[TestClass]
+public class ProjectActionsTests : TestBase
+{
+    [TestMethod]
+    public async Task SearchProjects_ReturnsProjects()
+    {
+		// Arrange
+		var actions = new ProjectActions(InvocationContext);
+        var request = new SearchProjectsRequest { };
+
+        // Act
+        var result = await actions.SearchProjects(request);
+
+        // Assert
+        PrintJsonResult(result);
+        Assert.IsNotNull(result);
+    }
+}
